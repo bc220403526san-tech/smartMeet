7f3248f2b5ae428252ba612a844b796f764138a2
Sana Arshad <bc220403526san@vu.edu.pk>
2026-09-02 22:02:03 +0500

    Add seeded admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,24 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::updateOrCreate(
+            [
+                'email' => 'sanaarshad@gmail.com',
+            ],
+            [
+                'name' => 'Sana',
+                'password' => 'SAna1234@@',
+                'role' => 'admin',
+                'is_active' => true,
+                'email_verified_at' => now(),
+            ]
+        );
     }
 }
